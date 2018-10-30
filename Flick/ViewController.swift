00c06d2531eb7614c3ab0e4067a5ec50b72532d2
Khang Nguyen <nkhang2112@yahoo.com.vn>
2018-10-30 16:34:55 -0700

    Initial Commit

---
+++ Flick/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flick
+//
+//  Created by Khang Nguyen on 10/30/18.
+//  Copyright © 2018 Khang Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
